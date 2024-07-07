ad61052f7287258d7925366444357bd9d23df38a
vlyrdv <61351039+vlyrdv@users.noreply.github.com>
2024-07-07 07:01:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ from CarNumbers.start import EditImage
 
 result = EditImage()
 
-for i in range(1, 13):
-    print(i)
-    result.draw_normal_img(f"{i}.jpg", max_power=True)
-    result.edit_form()
+result.draw_normal_img("IMAGE-PATH", max_power=True)
+result.edit_form()
 
 
